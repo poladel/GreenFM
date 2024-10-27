793986133e2e0cdd884e8068caf1203db71f520f
pola <plscachedel@gmail.com>
2024-10-27 20:19:00 +0800

    Updated Back Button for Log In and Register

--- public/js/global.js
+++ public/js/global.js
@@ -15,3 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+function goBack() {
+    const referrer = document.referrer;
+    if (referrer && !referrer.includes('/Register')) {
+      window.history.back();
+    } else {
+      window.location.href = '/'; // Redirect to homepage or another specific page
+    }
+  }
