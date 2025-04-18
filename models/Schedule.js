fc3b8a1fe0e77f10586f69bec664fcb73561c4ea
pola <plscachedel@gmail.com>
2025-04-18 13:43:55 +0800

    refactor: restructure schedule schema to include detailed staff and show information

--- models/Schedule.js
+++ models/Schedule.js
@@ -3,9 +3,41 @@ const mongoose = require('mongoose');
 const scheduleSchema = new mongoose.Schema({
     day: String,
     time: String,
-    title: String,
-    description: String,
     schoolYear: String,
+    showDetails: {
+        title: { type: String, required: true },
+        type: { type: [String], required: true },
+        description: { type: String, required: true },
+        objectives: { type: String, required: true },
+    },
+    executiveProducer: {
+        lastName: { type: String, required: true },
+        firstName: { type: String, required: true },
+        mi: String,
+        suffix: String,
+        cys: String,
+    },
+    hosts: [{
+        lastName: { type: String, required: true },
+        firstName: { type: String, required: true },
+        mi: String,
+        suffix: String,
+        cys: String,
+    }],
+    technicalStaff: [{
+        lastName: { type: String, required: true },
+        firstName: { type: String, required: true },
+        mi: String,
+        suffix: String,
+        cys: String,
+    }],
+    creativeStaff: {
+        lastName: { type: String, required: true },
+        firstName: { type: String, required: true },
+        mi: String,
+        suffix: String,
+        cys: String,
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Schedule', scheduleSchema);
