35dddb016660f46b7732677cd1cb948ae8bb63c3
sopiya <sophialaurendane@gmail.com>
2025-04-25 15:36:34 +0800

    fixed chat admin route

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -8,6 +8,7 @@ const adminRoute = [
     { path: '/Manage-Accounts', view: '1-admin/1-accounts', pageTitle: 'Manage Accounts', headerTitle: 'MANAGE ACCOUNTS', cssFile: 'css/admin-account.css', roles: ['Admin', 'Staff'], restricted: true },
     { path: '/Blocktimer', view: '1-admin/2-blocktimer', pageTitle: 'Blocktimer', headerTitle: 'BLOCKTIMER', cssFile: 'css/admin-blocktimer.css', roles: ['Admin', 'Staff'], restricted: true },
     { path: '/Staff', view: '1-admin/3-staff', pageTitle: 'Staff', headerTitle: 'STAFF', cssFile: 'css/admin-staff.css', roles: ['Admin', 'Staff'], restricted: true },
+    { path: '/Chat', view: '1-admin/4-chat', pageTitle: 'Chat', headerTitle: 'CHAT', cssFile: 'css/chat.css', roles: ['Admin', 'Staff'], restricted: true },
 ];
 
 // Define the routes and render views with dynamic titles
