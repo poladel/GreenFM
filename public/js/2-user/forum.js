729452f61e760c8ac534daccab3d07c7ba61f48e
Ron <ronjohn.cereno5@gmail.com>
2025-04-29 08:09:39 +0800

    Update

--- public/js/2-user/forum.js
+++ public/js/2-user/forum.js
@@ -175,7 +175,7 @@ document.addEventListener('DOMContentLoaded', function () {
         showToast('❌ Error creating post', 'error');
       }
     }
-    
+      
 
     async loadPosts(page = 1) {
       try {
